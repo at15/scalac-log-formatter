5f9dfec18fa9c66f55253433bfb56b4cb3f7098d
at15 <at15@outlook.com>
2015-05-30 14:45:24 +0800

    We can open log file now

--- src/main/scala/org/dy/lint/Log.scala
+++ src/main/scala/org/dy/lint/Log.scala
@@ -1,10 +1,20 @@
 package org.dy.lint
 
+import io.Source
+import fastparse._
+
 /**
  * Created by Pillar on 2015/5/30.
  */
 object Log {
   def main(args: Array[String]) = {
     println("Log lalala")
+    // open the log file
+    val logFile = "logs/linter-me-package.log"
+    val source = Source.fromFile(logFile,"UTF-8") // though the log is GBK, but only english, so
+    val linteIterator = source.getLines()
+    for(oneLine <- linteIterator) {
+      println(oneLine)
+    }
   }
 }
